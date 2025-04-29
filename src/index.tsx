daab45720a0c10cd054584a9a7824183733b350d
bijo-at-688165709689 <bijo@cordovacloud.com>
2025-04-29 16:53:39 +0100

    Add new review support.

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import UpcomingMovies from "./pages/upcomingMovies";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage';
 
 
 
@@ -38,6 +39,7 @@ const App = () => {
         <Route path="*" element={<Navigate to="/" />} />
         <Route path="/reviews/:id" element={<MovieReviewPage/>} />
         <Route path="/movies/upcoming" element={<UpcomingMovies/>} />
+        <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
       </Routes> 
         </MoviesContextProvider>
       </BrowserRouter>
--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -78,3 +78,11 @@ export interface BaseMovieProps {
     results: BaseMovieProps[];
   }
   
+
+  export interface Review {
+    author: string,
+    content: string,
+    agree: boolean,
+    rating: number,
+    movieId: number,
+  }
